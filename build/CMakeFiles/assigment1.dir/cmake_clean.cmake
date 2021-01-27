8ed0dae58ac850dd3d64f685e86ba6266a786ceb
JoeSteeb <josteeb@gmail.com>
2021-01-26 19:00:57 -0800

    Fixed typo

---
+++ build/CMakeFiles/assigment1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assigment1.dir/main.cpp.o"
+  "assigment1.pdb"
+  "assigment1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assigment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
