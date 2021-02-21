9dcd078262c516ca140d3a78f21866245e72edd7
JoeSteeb <josteeb@gmail.com>
2021-02-20 20:55:20 -0800

    added test functions

---
+++ testQueue.cpp
@@ -0,0 +1,27 @@
+#include "testQueue.h"
+#include <iostream>
+
+void test::dequeue()
+{
+
+}
+void test::enqueue()
+{
+
+}
+void test::peek()
+{
+    
+}
+void test::size()
+{
+
+}
+void test::isEmpty()
+{
+
+}
+void test::isFull()
+{
+
+}
--- testQueue.h
+++ testQueue.h
@@ -1,4 +1,13 @@
 #ifndef QUEUE_TEST_H
 #define QUEUE_TEST_H
+namespace test
+{
+    void dequeue();
+    void enqueue();
+    void peek();
+    void size();
+    void isEmpty();
+    void isFull();
+}
 
 #endif // !QUEUE_TEST_H
