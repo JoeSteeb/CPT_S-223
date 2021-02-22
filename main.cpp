fa0aa1486fda239dc74a205b41338888ba8cf124
Jstalin99 <53073571+Jstalin99@users.noreply.github.com>
2021-02-21 21:17:39 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@
 #include "testQueue.h"
 //#include "queue.h"
 
+/*
+Attributes 
+1)    The queue class and its functions should have a separate header file and be called in the main.
+2)    Comments on queue functions has to be more descriptive.
+3)    The destructor function should be delete[] arr or A memory leak will occur.
+4)    In the enqueue function The item should be added at the rear not the size
+5)    For the function isFull capacity should be returned at size, not size() – 1
+6)    As far as I can tell, the most efficient algorithms have been used for the functions.
+*/
+
 // main function
 int main()
 {
